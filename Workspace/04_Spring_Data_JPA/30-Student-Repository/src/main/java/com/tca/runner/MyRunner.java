1217e1fa1dd0dad371b60762585f7033aabcf487
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2026-08-25 17:44:46 +0530

    StudentRepository

--- Workspace/04_Spring_Data_JPA/30-Student-Repository/src/main/java/com/tca/runner/MyRunner.java
+++ Workspace/04_Spring_Data_JPA/30-Student-Repository/src/main/java/com/tca/runner/MyRunner.java
@@ -19,9 +19,10 @@ public class MyRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        Student s = new Student(101,"Dnyaneshwar Padule", 90.23, "Pune", Gender.MALE, LocalDate.of(2006,5,31));
-        s = studentService.save(s);
+//        Student s = new Student(101,"Dnyaneshwar Padule", 90.23, "Pune", Gender.MALE, LocalDate.of(2006,5,31));
+        Student s = new Student(102,"Athrva Gheware", 91.40, "Sangli", Gender.MALE, LocalDate.of(2006,11,21));
 
+        s = studentService.save(s);
         System.out.println("Student is saved successfully !");
     }
 }
