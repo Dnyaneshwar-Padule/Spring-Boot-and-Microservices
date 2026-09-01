92e24dc39d6c09d1e4298ff9ffdec9a8ce3650ba
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2026-09-01 17:14:10 +0530

    StudentRepository with JPQL Query and Native Query

--- Workspace/04_Spring_Data_JPA/32-Student-Repository-With-JPQL-And-Native-Queries/src/main/java/com/tca/repository/StudentRepository.java
+++ Workspace/04_Spring_Data_JPA/32-Student-Repository-With-JPQL-And-Native-Queries/src/main/java/com/tca/repository/StudentRepository.java
@@ -42,7 +42,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.id = :id")
     public Optional<Student> fetchById(@Param("id") Integer id);
 
-    @Query(name="SELECT * FROM STUDENT WHERE ID = :id", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE ID = :id", nativeQuery = true)
     public Optional<Student> getByID(@Param("id") Integer id);
 
 
@@ -50,7 +50,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.city LIKE :city")
     public List<Student> fetchByCity(@Param("city") String city);
 
-    @Query(name="SELECT * FROM STUDENT WHERE CITY LIKE :city", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE CITY LIKE :city", nativeQuery = true)
     public List<Student> getByCity(@Param("city") String city);
 
 
@@ -58,7 +58,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.gender = :gender")
     public List<Student> fetchByGender(Gender gender);
 
-    @Query(name="SELECT * FORM STUDENT WHERE GENDER = :gender", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE GENDER = :gender", nativeQuery = true)
     public List<Student> getByGender(@Param("gender") Gender gender);
 
 
@@ -69,7 +69,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
             @Param("to") Double toPer
     );
 
-    @Query(name="SELECT * FROM STUDENT WHERE PER BETWEEN :from AND :to", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE PER BETWEEN :from AND :to", nativeQuery = true)
     public List<Student> getByPerBetween(
             @Param("from") Double fromPer,
             @Param("to") Double toPer
@@ -83,7 +83,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
             @Param("per") Double per
     );
 
-    @Query(name="SELECT * FROM STUDENT WHERE CITY LIKE :city AND PER > :per", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE CITY LIKE :city AND PER > :per", nativeQuery = true)
     public List<Student> getByCityAndPerGreaterThan(
             @Param("city") String city,
             @Param("per") Double per
@@ -105,7 +105,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
             @Param("per") Double per
     );
 
-    @Query(name="SELECT * FROM STUDENT WHERE GENDER = :gender AND PER >= :per", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE GENDER = :gender AND PER >= :per", nativeQuery = true)
     public List<Student> getByGenderAndPerGreaterThanEqual(
             @Param("gender") Gender gender,
             @Param("per") Double per
@@ -116,7 +116,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.name LIKE CONCAT(:namePrefix, '%') ")
     public List<Student> fetchByNameStartingWith(@Param("namePrefix") String namePrefix);
 
-    @Query( name="SELECT * FROM STUDENT WHERE NAME LIKE CONCAT(:namePrefix, '%')", nativeQuery = true)          // I am using mariadb so it will work for now !!
+    @Query( value="SELECT * FROM STUDENT WHERE NAME LIKE CONCAT(:namePrefix, '%')", nativeQuery = true)          // I am using mariadb so it will work for now !!
     public List<Student> getByNameStartingWith(@Param("namePrefix") String namePrefix);
 
 
@@ -143,7 +143,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
             @Param("to") LocalDate toBirthDate
     );
 
-    @Query(name="SELECT * FROM STUDENT WHERE BIRTHDATE BETWEEN :from and :to", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE BIRTH_DATE BETWEEN :from and :to", nativeQuery = true)
     public List<Student> getByBirthDateBetween(
             @Param("from") LocalDate fromBirthDate,
             @Param("to") LocalDate toBirthDate
@@ -154,7 +154,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE lower(s.name) LIKE CONCAT('%', concat(:name ,'%') ) ")
     public List<Student> fetchByNameContainingIgnoreCase(@Param("name") String name);
 
-    @Query(name="SELECT * FROM STUDENT WHERE LOWER(NAME) LIKE CONCAT('%', CONCAT(:name, '%'))", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE LOWER(NAME) LIKE CONCAT('%', CONCAT(:name, '%'))", nativeQuery = true)
     public List<Student> getByNameContainingIgnoreCase(@Param("name") String name);
 
 
@@ -162,7 +162,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.gender IN :genders")
     public List<Student> fetchByGenderIn(@Param("genders") List<Gender> genders);
 
-    @Query(name = "SELECT * FORM STUDENT WHERE GENDER IN :genders", nativeQuery = true)
+    @Query(name = "SELECT * FROM STUDENT WHERE GENDER IN :genders", nativeQuery = true)
     public List<Student> getByGenderIn(@Param("genders") List<Gender> genders);
 
 
@@ -173,7 +173,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
             @Param("per") Double per
     );
 
-    @Query(name="SELECT * FROM STUDENTS WHERE ID IN :id AND PER > :per",nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENTS WHERE ID IN :ids AND PER > :per",nativeQuery = true)
     public List<Student> getByIdInAndPerGreaterThan(
             @Param("ids") List<Integer> ids,
             @Param("per") Double per
@@ -183,14 +183,14 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.city NOT IN :cities")
     public List<Student> fetchByCityNotIn(@Param("cities") List<String> cities);
 
-    @Query(name="SELECT * FROM STUDENT WHERE CITY NOT IN :cities", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE CITY NOT IN :cities", nativeQuery = true)
     public List<Student> getByCityNotIn(@Param("cities") List<String> cities);
 
     /* Find students whose city is NULL. */
     @Query("SELECT s FROM Student s WHERE s.city IS NULl")
     public List<Student> fetchByCityIsNull();
 
-    @Query(name="SELECT * FROM STUDENT WHERE CITY IS NULL", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE CITY IS NULL", nativeQuery = true)
     public List<Student> getByCityIsNull();
 
 
@@ -198,7 +198,7 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT s FROM Student s WHERE s.city IS NOT NULl")
     public List<Student> fetchByCityIsNotNull();
 
-    @Query(name="SELECT * FROM STUDENT WHERE CITY IS NOT NULL", nativeQuery = true)
+    @Query(value="SELECT * FROM STUDENT WHERE CITY IS NOT NULL", nativeQuery = true)
     public List<Student> getByCityIsNotNull();
 
 
@@ -222,14 +222,14 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
     @Query("SELECT MAX(s.per) FROM Student s")
     public Double fetchMaxPer();
 
-    @Query(name="SELECT MAX(PER) FROM STUDENT", nativeQuery = true)
+    @Query(value="SELECT MAX(PER) FROM STUDENT", nativeQuery = true)
     public Double getMaxPer();
 
     /* Find the lowest percentage. */
     @Query("SELECT MIN(s.per) FROM Student s")
     public Double fetchMinPer();
 
-    @Query(name="SELECT MIN(PER) FROM STUDENT", nativeQuery = true)
+    @Query(value="SELECT MIN(PER) FROM STUDENT", nativeQuery = true)
     public Double getMinPer();
 
 
@@ -251,20 +251,179 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     /* Find the average percentage of male students. */
     @Query("SELECT AVG(s.per) FROM Student  s WHERE s.gender = :gender")
-    public Double fetchAveragePerByGender(Gender gender);
+    public Double fetchAveragePerByGender(@Param("gender") Gender gender);
 
     @Query(name = "SELECT AVG(PER) FROM STUDENT  WHERE GENDER = :gender", nativeQuery = true)
-    public Double getAveragePerByGender(Gender gender);
+    public Double getAveragePerByGender(@Param("gender") Gender gender);
 
-    
+
+    /* Find the number of students in each city
+    Expected conceptual result:
+    Pune      → 15
+    Mumbai    → 23
+    Delhi     → 8
+    */
+    @Query("SELECT s.city, COUNT(s) FROM Student s GROUP BY s.city")
+    public List<Object[]> fetchStudentCountInEachCity();
+
+    @Query(value="SELECT CITY, COUNT(*) FROM STUDENT GROUP BY CITY", nativeQuery = true)
+    public List<Object[]> getStudentCountInEachCity();
+
+    /* Find the average percentage for each city. */
+   @Query("SELECT s.city, AVG(s.per) FROM Student s GROUP BY s.city")
+    public List<Object[]> fetchAveragePerWithCity();
+
+    @Query(value="SELECT CITY, AVG(PER) FROM STUDENT GROUP BY CITY", nativeQuery = true)
+    public List<Object[]> getAveragePerWithCity();
+
+    /* Find the number of male and female students separately*/
+    @Query("SELECT s.gender, COUNT(s) FROM Student s GROUP BY s.gender")
+    public List<Object[]> fetchCountByGender();
+
+    @Query(value="SELECT GENDER, COUNT(*) FROM STUDENT GROUP BY GENDER", nativeQuery = true)
+    public List<Object[]> getCountByGender();
+
+
+    /* Find the highest percentage in each city.*/
+    @Query("SELECT s.city, MAX(s.per) FROM Student s GROUP BY s.city")
+    public List<Object[]> fetchMaxPerWithEachCity();
+
+    @Query(name = "SELECT CITY, MAX(PER) FROM STUDENT GROUP BY CITY", nativeQuery = true)
+    public List<Object[]> getMaxPerWithEachCity();
+
+
+    /* Find cities having more than 10 students.*/
+    @Query("SELECT s.city FROM Student s GROUP BY s.city HAVING COUNT(s) > :count")
+    public List<String> fetchCitiesWithStudentCount(@Param("count") Integer requiredStudentCount);
+
+    @Query(value="SELECT CITY FROM STUDENT GROUP BY CITY HAVING COUNT(*) > :count", nativeQuery = true)
+    public List<String> getCitiesWithStudentCount(@Param("count") Integer requiredStudentCount);
+
+
+    /* Find cities whose average student percentage is greater than 70.*/
+    @Query("SELECT s.city FROM Student s GROUP BY s.city HAVING AVG(s.per) > :per")
+    public List<String> fetchCitiesWithAvgPerGreaterThan(@Param("per") Double per);
+
+    @Query(value="SELECT CITY FROM STUDENT GROUP BY CITY HAVING AVG(PER) > :per", nativeQuery = true)
+    public List<String> getCitiesWithAvgPerGreaterThan(@Param("per") Double per);
+
+
+    /* Find genders having more than 20 students.*/
+    @Query("SELECT s.gender FROM Student s GROUP BY s.gender HAVING COUNT(s) > :count")
+    public List<Gender> fetchGenderWithCountGreaterThan(@Param("count") Integer count);
+
+    @Query(value="SELECT GENDER FROM STUDENT GROUP BY GENDER HAVING COUNT(*) > :count", nativeQuery = true)
+    public List<Gender> getGenderWithCountGreaterThan(@Param("count") Integer count);
+
+
+    /*    Find students whose percentage is greater than the average percentage of all students. */
+    @Query("SELECT s FROM Student s WHERE s.per > (SELECT AVG(s1.per) FROM Student s1 ) ")
+    public List<Student> fetchByPerGreaterThanAvgPer();
+
+    @Query(value="SELECT * FROM STUDENT WHERE PER > (SELECT AVG(PER) FROM STUDENT)", nativeQuery = true)
+    public List<Student> getByPerGreaterThanAvgPer();
+
+
+    /*  Find students whose percentage is equal to the highest percentage in the college. */
+    @Query("SELECT s FROM Student s WHERE s.per = (SELECT MAX(s1.per) FROM Student s1)")
+    public List<Student> fetchByPerEqualMaxPer();
+
+    @Query(value="SELECT * FROM STUDENT WHERE PER = (SELECT MAX(PER) FROM STUDENT)", nativeQuery = true)
+    public List<Student> getByPerEqualMaxPer();
+
+
+    /* Find students who live in a city whose average percentage is greater than 75. */
+    @Query("SELECT s FROM Student s WHERE s.city IN (SELECT s1.city FROM Student s1 GROUP BY s1.city HAVING AVG(s1.per) > :avgPer)")
+    public List<Student> fetchByCityWithAvgPerGreaterThan(@Param("avgPer") Double avgPer);
+
+    @Query(value="SELECT * FROM STUDENT WHERE CITY IN (SELECT CITY FROM STUDENT GROUP BY CITY HAVING AVG(PER) > :avgPer)", nativeQuery = true)
+    public List<Student> getByCityWithAvgPerGreaterThan(@Param("avgPer") Double avgPer);
+
+    /*    Find students whose percentage is greater than the average percentage of students from their own city.
+        101 AAA 90 Pune
+        102 BBB 80 Mumbai
+        103 CCC 85 Mumbai
+        104 DDD 70 Panji
+        105 EEE 85 Pune
+        106 FFF 95 Pune
+
+        Pune    90
+        Mumbai  80
+        Panji   70
+
+        select s1.id, s1.name, s1.per, s1.city, s1.birth_date
+        from student s1
+        join (
+            select city, avg(per) as avg_per
+            from student
+            group by city
+        ) as s2
+        on s1.city = s2.city
+        where s1.per > s2.avg_per;
+     */
+    @Query("SELECT s FROM Student s WHERE s.per > (SELECT AVG(s2.per) FROM Student s2 WHERE s2.city = s.city)")
+    public List<Student> fetchByPerGreaterThanAvgPerOfSameCity();
+
+    @Query(value="SELECT s1.ID, s1.NAME, s1.PER, s1.CITY, s1.GENDER, s1.BIRTH_DATE " +
+            "FROM STUDENT s1 " +
+            "JOIN " +
+            "   (SELECT city, AVG(PER) as avg_per FROM STUDENT GROUP BY CITY) s2 " +
+            "ON s1.city = s2.city " +
+            "WHERE s1.per > s2.avg_per",
+            nativeQuery = true
+    )
+    public List<Student> getByPerGreaterThanAvgPerOfSameCity();
+
+    /* Find the student(s) having the second-highest percentage. */
+    @Query("SELECT s " +
+            "FROM Student s " +
+            "WHERE s.per = (" +
+            "   SELECT MAX(s2.per) " +
+            "   FROM Student s2 " +
+            "   WHERE s2.per != (" +
+            "       SELECT MAX(s3.per) " +
+            "       FROM Student s3" +
+            "   )" +
+            ") ")
+    public List<Student> fetchSecondHighestStudents();
+
+    @Query(value="SELECT * FROM STUDENT WHERE PER = " +
+            "   (SELECT MAX(PER) FROM STUDENT WHERE PER != " +
+            "       (SELECT MAX(PER) FROM STUDENT) " +
+            "   )",
+            nativeQuery = true
+    )
+    public List<Student> getSecondHighestStudents();
+
+
     /*
     Find students whose percentage is greater than the average percentage of students living in their own city,
     but only consider cities having at least 5 students.
 
-    select * from student where per > (
-        select
-    )
+        select s1.id, s1.name, s1.per, s1.city, s1.birth_date
+        from student s1
+        join (
+            select city, avg(per) as avg_per, count(*) as count
+            from student
+            group by city
+        ) as s2
+        on s1.city = s2.city
+        where s1.per > s2.avg_per
+        and count >= studentCount;
 
      */
+    @Query("SELECT s FROM Student s WHERE s.per > (SELECT AVG(s2.per) FROM Student s2 WHERE s2.city = s.city AND s2.city IN (SELECT s3.city FROM Student s3 GROUP BY s3.city HAVING COUNT(s3) >= :studentCount))")
+    public List<Student> fetchByPerGreaterThanAvgPerOfSameCityHavingStudentCountGreaterThanEqual(@Param("studentCount") Integer studentCount);
+
+    @Query(value="SELECT s1.ID, s1.NAME, s1.PER, s1.CITY, s1.GENDER, s1.BIRTH_DATE " +
+            "FROM STUDENT s1 " +
+            "JOIN " +
+            "   (SELECT city, AVG(PER) as avg_per, COUNT(*) as count FROM STUDENT GROUP BY CITY) s2 " +
+            "ON s1.city = s2.city " +
+            "WHERE s1.per > s2.avg_per " +
+            "AND s2.count >= :studentCount",
+            nativeQuery = true
+    )
+    public List<Student> getByPerGreaterThanAvgPerOfSameCityHavingStudentCountGreaterThanEqual(@Param("studentCount") Integer studentCount);
 
 }
