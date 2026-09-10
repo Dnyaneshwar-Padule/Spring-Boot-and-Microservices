56e0a31b46ebc67624294617196a29f23b0c2e5a
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2026-09-11 00:10:40 +0530

    Spring Data JPA: key generation strategies : TABLE

--- Workspace/04_Spring_Data_JPA/36-Key-Generation-with-Table-Strategy/src/main/java/com/tca/entity/Student.java
+++ Workspace/04_Spring_Data_JPA/36-Key-Generation-with-Table-Strategy/src/main/java/com/tca/entity/Student.java
@@ -37,13 +37,19 @@ public class Student {
     @Id
     private Long id;
 
+    @Column(name="NAME", nullable = false, length = 48)
     private String name;
 
+    @Column(name="PER", columnDefinition = "FLOAT(PER >= 0 AND per <= 100)")
     private Double per;
 
+    @Column(name="CITY", nullable = false, length = 48)
     private String city;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name="GENDER", nullable = false)
     private Gender gender;
 
+    @Column(name="BIRTH_DATE", nullable = false)
     private LocalDate birthDate;
 }
