b2cc75f8d73b52fed965cd84e20b8c9f464723eb
Dnyaneshwar-Padule <dnyaneshwarpadule83@gmail.com>
2026-09-14 22:14:57 +0530

    Spring Data JPA: One-To-Many Unidirectional

--- Workspace/04_Spring_Data_JPA/39-One-To-Many-Unidirectional/src/main/java/com/tca/runner/MyRunner.java
+++ Workspace/04_Spring_Data_JPA/39-One-To-Many-Unidirectional/src/main/java/com/tca/runner/MyRunner.java
@@ -4,12 +4,14 @@ import com.tca.entity.Customer;
 import com.tca.entity.Order;
 import com.tca.entity.OrderStatus;
 import com.tca.service.CustomerService;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class MyRunner implements ApplicationRunner {
@@ -22,16 +24,21 @@ public class MyRunner implements ApplicationRunner {
 
 
     @Override
+    @Transactional
     public void run(ApplicationArguments args) throws Exception {
-        Customer customer = new Customer();
-        customer.setAddress("Pune");
-        customer.setEmail("dummy@demo.com");
-        customer.setName("Vishwa");
-        customer.setOrders(List.of(
-                new Order(101L, 450.0, "Paneer Tikka", OrderStatus.ORDERED, LocalDate.now()),
-                new Order(102L, 500.0, "Chicken Hundi", OrderStatus.DELIVERED, LocalDate.now())
-        ) );
+//        Customer customer = new Customer();
+//        customer.setAddress("Pune");
+//        customer.setEmail("dummy@demo.com");
+//        customer.setName("Vishwa");
+//        customer.setOrders(List.of(
+//                new Order(101L, 450.0, "Paneer Tikka", OrderStatus.ORDERED, LocalDate.now()),
+//                new Order(102L, 500.0, "Chicken Hundi", OrderStatus.DELIVERED, LocalDate.now())
+//        ) );
+//        customerService.save(customer);
 
-        customerService.save(customer);
+        Optional<Customer> customer = customerService.getById(3L);
+        customer.ifPresent(System.out::println);
+        customer.ifPresent(customerService::delete);
+
     }
 }
